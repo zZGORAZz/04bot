a1f2bf1c2aba26d1a8903b0fcb32bad82ae635de
zZGORAZz <76191329+zZGORAZz@users.noreply.github.com>
2020-12-29 00:23:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
     if(message.content === '``04서버주소') {
-        message.reply('34.64.126.27');
+        message.reply('04서버.kro.kr');
     }
 });
 
